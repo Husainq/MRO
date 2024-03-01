e655db75596d086bb426eee38c5d60f707ce6dab
wirayudha23 <ahmad21si@mahasiswa.pcr.ac.id>
2024-03-01 15:27:32 +0700

    Update SplashActivity.kt

--- app/src/main/java/com/example/mro/MainActivity.kt
+++ app/src/main/java/com/example/mro/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.mro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.mro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
