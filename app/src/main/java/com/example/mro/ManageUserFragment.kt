05656d1bd533614caf2887f597dfa5d28008fd17
husainq <qalbi21si@mahasiswa.pcr.ac.id>
2024-03-07 15:21:14 +0700

    Penambahan layout administrator

--- app/src/main/java/com/example/mro/ManageUserFragment.kt
+++ app/src/main/java/com/example/mro/ManageUserFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AddUserFragment.newInstance] factory method to
+ * Use the [ManageUserFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AddUserFragment : Fragment() {
+class ManageUserFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class AddUserFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_user, container, false)
+        return inflater.inflate(R.layout.fragment_manage_user, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class AddUserFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AddUserFragment.
+         * @return A new instance of fragment ManageUserFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AddUserFragment().apply {
+            ManageUserFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
