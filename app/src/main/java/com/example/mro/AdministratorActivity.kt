5ead3d6e642dba027e5c25afa011cfd41fb26ad6
husainq <qalbi21si@mahasiswa.pcr.ac.id>
2024-03-20 09:10:16 +0700

    Backend

--- app/src/main/java/com/example/mro/AdministratorActivity.kt
+++ app/src/main/java/com/example/mro/AdministratorActivity.kt
@@ -2,10 +2,30 @@ package com.example.mro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.mro.databinding.ActivityAdministratorBinding
 
 class AdministratorActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAdministratorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_administrator)
+        binding = ActivityAdministratorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        changeFragment(BerandaFragment())
+        binding.navbar.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.beranda -> changeFragment(BerandaAdministratorFragment())
+                R.id.profil -> changeFragment(ProfilAdministratorFragment())
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container, fragment)
+        fragmentTransaction.commit()
     }
 }
