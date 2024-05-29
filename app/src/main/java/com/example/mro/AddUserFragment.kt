fbbfb4eafe6ec01b319ae62761296945050406fc
husainq <qalbi21si@mahasiswa.pcr.ac.id>
2024-05-29 08:19:18 +0700

    Mng User

--- app/src/main/java/com/example/mro/AddUserFragment.kt
+++ app/src/main/java/com/example/mro/AddUserFragment.kt
@@ -86,11 +86,7 @@ class AddUserFragment : Fragment(), View.OnClickListener {
         val role = binding.edtRoleAdd.selectedItem.toString()
         val password = binding.edtPasswordAdd.text.toString().trim()
 
-<<<<<<< HEAD
         if (username.isEmpty() || email.isEmpty() || role.isEmpty() || password.isEmpty()) {
-=======
-        if (username.isEmpty() || email.isEmpty() || role.isEmpty() || password.isEmpty() || selectedImageUri == null) {
->>>>>>> 98c38e7a3aa61f28927d30455ced2a8286c30239
             Toast.makeText(requireContext(), "Isi data secara lengkap tidak boleh kosong", Toast.LENGTH_SHORT).show()
             return
         }
@@ -101,7 +97,6 @@ class AddUserFragment : Fragment(), View.OnClickListener {
                     val user = auth.currentUser
                     val userId = user?.uid
 
-<<<<<<< HEAD
                     if (selectedImageUri != null) {
                         val storageRef = storage.reference
                         val profileImageRef = storageRef.child("profile_images/${UUID.randomUUID()}/${UUID.randomUUID()}.jpg")
@@ -111,50 +106,13 @@ class AddUserFragment : Fragment(), View.OnClickListener {
                                 profileImageRef.downloadUrl.addOnSuccessListener { uri ->
                                     val imageUrl = uri.toString()
                                     saveUserToDatabase(userId, username, email, role, password, imageUrl)
-=======
-                    val storageRef = storage.reference
-                    val profileImageRef = storageRef.child("profile_images/${UUID.randomUUID()}.jpg")
-
-                    selectedImageUri?.let {
-                        profileImageRef.putFile(it)
-                            .addOnSuccessListener {
-                                profileImageRef.downloadUrl.addOnSuccessListener { uri ->
-                                    val imageUrl = uri.toString()
-
-                                    val database = FirebaseDatabase.getInstance()
-                                    val reference = database.getReference("User")
-
-                                    val userData = HashMap<String, Any>()
-                                    userData["email"] = email
-                                    userData["username"] = username
-                                    userData["role"] = role
-                                    userData["password"] = password
-                                    userData["profileImageUrl"] = imageUrl
-
-                                    userId?.let {
-                                        reference.child(it).setValue(userData)
-                                            .addOnSuccessListener {
-                                                Toast.makeText(requireContext(), "Pendaftaran berhasil!", Toast.LENGTH_SHORT).show()
-                                                val mngUserFragment = MngUserFragment()
-                                                val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
-                                                transaction.replace(R.id.container, mngUserFragment)
-                                                transaction.commit()
-                                            }
-                                            .addOnFailureListener { e ->
-                                                Toast.makeText(requireContext(), "Gagal menyimpan data pengguna: ${e.message}", Toast.LENGTH_SHORT).show()
-                                            }
-                                    }
->>>>>>> 98c38e7a3aa61f28927d30455ced2a8286c30239
                                 }
                             }
                             .addOnFailureListener { e ->
                                 Toast.makeText(requireContext(), "Gagal mengunggah gambar: ${e.message}", Toast.LENGTH_SHORT).show()
                             }
-<<<<<<< HEAD
                     } else {
                         saveUserToDatabase(userId, username, email, role, password, null)
-=======
->>>>>>> 98c38e7a3aa61f28927d30455ced2a8286c30239
                     }
                 } else {
                     val exception = task.exception
@@ -163,7 +121,6 @@ class AddUserFragment : Fragment(), View.OnClickListener {
             }
     }
 
-<<<<<<< HEAD
     private fun saveUserToDatabase(userId: String?, username: String, email: String, role: String, password: String, imageUrl: String?) {
         val database = FirebaseDatabase.getInstance()
         val reference = database.getReference("User")
@@ -192,8 +149,6 @@ class AddUserFragment : Fragment(), View.OnClickListener {
         }
     }
 
-=======
->>>>>>> 98c38e7a3aa61f28927d30455ced2a8286c30239
     companion object {
         private const val IMAGE_PICK_REQUEST = 100
     }
--- app/src/main/java/com/example/mro/MngUserFragment.kt
+++ app/src/main/java/com/example/mro/MngUserFragment.kt
@@ -15,21 +15,14 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-<<<<<<< HEAD
 import androidx.activity.result.contract.ActivityResultContracts
 
 class MngUserFragment : Fragment() {
     private lateinit var binding: FragmentMngUserBinding
-=======
-
-class MngUserFragment : Fragment() {
-    lateinit var binding: FragmentMngUserBinding
->>>>>>> 98c38e7a3aa61f28927d30455ced2a8286c30239
     private lateinit var userList: MutableList<User>
     private lateinit var ref: DatabaseReference
     private lateinit var userAdapter: UserAdapter
 
-<<<<<<< HEAD
     private val pickImageLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         uri?.let {
             userAdapter.updateProfilePicture(it)
@@ -48,12 +41,6 @@ class MngUserFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-=======
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
->>>>>>> 98c38e7a3aa61f28927d30455ced2a8286c30239
         binding = FragmentMngUserBinding.inflate(inflater, container, false)
 
         binding.backUser.setOnClickListener {
@@ -80,11 +67,7 @@ class MngUserFragment : Fragment() {
 
         ref.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-<<<<<<< HEAD
                 if (isAdded) {
-=======
-                if (isAdded) { // Pastikan Fragment terpasang sebelum menggunakan requireActivity()
->>>>>>> 98c38e7a3aa61f28927d30455ced2a8286c30239
                     if (snapshot.exists()) {
                         userList.clear()
                         for (a in snapshot.children) {
@@ -93,15 +76,7 @@ class MngUserFragment : Fragment() {
                                 userList.add(it)
                             }
                         }
-<<<<<<< HEAD
                         userAdapter = UserAdapter(requireActivity(), R.layout.detil_user, userList, this@MngUserFragment)
-=======
-                        userAdapter = UserAdapter(
-                            requireActivity(),
-                            R.layout.detil_user,
-                            userList
-                        )
->>>>>>> 98c38e7a3aa61f28927d30455ced2a8286c30239
                         binding.hasilUser.adapter = userAdapter
                     }
                 }
@@ -113,25 +88,7 @@ class MngUserFragment : Fragment() {
         })
     }
 
-<<<<<<< HEAD
     fun startImagePicker() {
         pickImageLauncher.launch("image/*")
-=======
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == UserAdapter.REQUEST_CODE_PICK_IMAGE && resultCode == Activity.RESULT_OK) {
-            val selectedImageUri: Uri? = data?.data
-            if (selectedImageUri != null) {
-                userAdapter.updateProfilePicture(selectedImageUri)
-
-                // Optionally, update the user's profile picture URI in Firebase
-                val dbAnggota = FirebaseDatabase.getInstance().getReference("User")
-                userAdapter.selectedUser?.let {
-                    it.profilePictureUri = selectedImageUri.toString()
-                    dbAnggota.child(it.userId).setValue(it)
-                }
-            }
-        }
->>>>>>> 98c38e7a3aa61f28927d30455ced2a8286c30239
     }
 }
